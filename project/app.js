da81b9b9bcd24c6938da36516e68dae131f2141d
francodiaz3010 <francoexequiel3010@gmail.com>
2024-06-14 21:46:37 -0300

    modificando app.js y home.html

--- project/app.js
+++ project/app.js
@@ -1,14 +1,13 @@
 const express=require('express');
 const path = require("node:path");
 const app= express();
-app.use(express.static("./public"));
-const PORT=process.env.PORT ?? 3030;
+
+
+
 app.get("/",(req, res)=>{
     res.sendFile(path.resolve("views/home.html"));
 });
 
-app.listen(PORT, (err)=>{
-    err
-    ?console.error("Servidor failed:",err.message)
-    : console.log(`Server running on http://localhost:&{PORT}`)
-})
+app.listen(3000, () => console.log("Server run: http://www.localhost:3000"))
+
+app.use(express.static("./public"));
