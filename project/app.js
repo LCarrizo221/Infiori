e12bbf617b156310f1324fc5e9c31f7861082471
Lcarrizo221 <lucascarrizo566@gmail.com>
2024-07-01 19:43:20 -0300

    incorporacion de carrito de compras

--- project/app.js
+++ project/app.js
@@ -19,3 +19,6 @@ app.get('/detailProduct',(req,res)=>{
 app.get("/login", (req,res) =>{
     res.sendFile(path.resolve("views/login.html"))
 })
+app.get('/shoppingCart',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/carrito-compra.html'));
+});
