19771d232b3d450e8d1f5e017808d8a6a4e0e042
Santiago Zalazar <santiagozalazar1@gmail.com>
2024-07-01 01:29:58 -0300

    agrego al package.json el test y el start

--- project/app.js
+++ project/app.js
@@ -2,16 +2,16 @@ const express=require('express');
 const path = require("node:path");
 const app= express();
 
-
+let port = 3000 || process.env.PORT;
 
 app.get("/",(req, res)=>{
     res.sendFile(path.resolve("views/home.html"));
 });
 
-app.listen(3000, (err) => console.log("Server run: http://www.localhost:3000"))
+app.listen(port, (err) => console.log(`Server run: http://localhost:${port}`))
 
 app.use(express.static("./public"));
 
-app.get('/views/detailProduct.html',(req,res)=>{
+app.get('/detailProduct',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./views/detailProduct.html'));
 });
