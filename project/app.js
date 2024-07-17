4e7da2ce8173e1bebce7ba97c8e92ce95dc0dee6
Lcarrizo221 <lucascarrizo566@gmail.com>
2024-07-16 21:04:30 -0300

    Hice el router y controller de products (cart y detail)

--- project/app.js
+++ project/app.js
@@ -2,6 +2,7 @@ const express=require('express');
 const path = require("node:path");
 const app= express();
 const homeRoute = require("./routes/homeRoute")
+const productsRoute = require("./routes/productsRoute")
 
 const port = 3000 || process.env.PORT;
 
@@ -11,21 +12,13 @@ app.listen(port, (err) => console.log(`Server run: http://localhost:${port}`))
 
 app.use(express.static("./public"));
 
-
 app.set ("view engine", "ejs");
+
+
+
+
+
+
+
 app.use("/", homeRoute)
-
-app.get('/detailProduct',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./views/detailProduct.html'));
-});
-
-app.get("/login", (req,res) =>{
-    res.sendFile(path.resolve("views/login.html"))
-})
-app.get('/shoppingCart',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./views/carrito-compra.html'));
-});
-
-app.get("/register", (req,res) =>{
-    res.sendFile(path.resolve("views/formulario.html"))
-})
+app.use("/product", productsRoute);
