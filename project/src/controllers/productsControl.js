d4601850f79442c157af9b92c78f3df09335df20
Santiago Notebook <santiagozalazar1@gmail.com>
2024-07-19 20:30:57 -0300

    Arreglo de Card en Details

--- project/src/controllers/productsControl.js
+++ project/src/controllers/productsControl.js
@@ -1,10 +1,12 @@
 const articles = require("../models/products.json")
+
+
 const product = {
     main: (req,res) =>{
         res.render("carrito-compra");
     },
     detail: (req,res) =>{
-        res.render("detail");
+        res.render("detail",{ articles });
     },
     edit: (req, res) =>{
         res.render("formEdit")
