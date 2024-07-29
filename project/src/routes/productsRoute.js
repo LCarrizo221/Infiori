0018d555d71796d31ef6ed8ad4df438dc56b3c74
Santiago Zalazar <santiagozalazar1@gmail.com>
2024-07-28 22:42:44 -0300

    Conf multer

--- project/src/routes/productsRoute.js
+++ project/src/routes/productsRoute.js
@@ -1,28 +1,14 @@
 const express = require("express")
 const router = express.Router()
 const productsControl = require("../controllers/productsControl");
+const Fileupload = require("../services/fileUpload");
 
-// Config multer for multimedia
-const multer = require('multer');
-
-let multerDiskStorage = multer.diskStorage({
-    destination:(req,file,callback)=>{
-      let folder = '/public/imageProductInfo'
-        callback(null,folder);
-    },
-    filename: (req,file,callback)=> {
-
-    callback(null, file.fieldname+'-'+path.extname(file.originalname));
-    }
-});
-
-const fileUpload = multer({ multerDiskStorage : multerDiskStorage });
 
 router.get("/cart", productsControl.main);
 router.get("/detail/:id", productsControl.detail);
 router.get("/edit",productsControl.edit);
 router.get("/upload", productsControl.upLoad);
-router.post("/upload/", fileUpload.single('imagen'), productsControl.upLoadImag);
+router.post("/upload/", Fileupload.single('imagen'), productsControl.upLoadImag);
 
 
 
