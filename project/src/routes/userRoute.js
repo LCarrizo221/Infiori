50de496dcd1171f527182523ec8800c7ce23e6d9
francodiaz3010 <francoexequiel3010@gmail.com>
2024-08-18 22:09:04 -0300

    Agregando

--- project/src/routes/userRoute.js
+++ project/src/routes/userRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const userValidations = require("../services/userValidations")
 
 router.get("/register", userController.showRegister);
 router.get("/login", userController.showLogin);
--- project/src/services/userValidations.js
+++ project/src/services/userValidations.js
@@ -5,5 +5,9 @@ const path = require('path');
 
 const userValidations = [
     body("userName").notEmpty().withMessage("Debe completar el campo de usuario."),
-    body("password").notEmpty().withMessage("Debe por favor completar el campo de contraseña."),
+    body("password").notEmpty().withMessage("Debe completar el campo de contraseña."),
 ];
+
+
+module.exports = userValidations;
+
