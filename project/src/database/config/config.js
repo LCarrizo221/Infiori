eb8260d821671dd4ce64dba9ca37d1209e74f748
Santiago Zalazar <santiagozalazar1@gmail.com>
2024-11-06 15:57:39 -0300

    modify config connect

--- project/src/database/config/config.js
+++ project/src/database/config/config.js
@@ -1,32 +1,30 @@
 module.exports = {
-"development": {
-    "username": "uw6fl0oeexsl0y7x",
-    "password": "QG9vrp0wsFsVkYAe0sbE",
-    "database": "bptswslxnt8j94cblpyh",
-    "host": "bptswslxnt8j94cblpyh-mysql.services.clever-cloud.com",
-    "dialect": "mysql"
-}
-  /*
-  "development": {
+  /*"development": {
     "username": "root",
     "password": "",
     "database": "infiori",
     "host": "127.0.0.1",
     "dialect": "mysql"
-  } */,
-  "test": {
-    "username": "root",
-    "password": "",
-    "database": "infiori",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": "",
-    "database": "infiori",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
-
+  }*/
+    "development": {
+      "username": "uw6fl0oeexsl0y7x",
+      "password": "QG9vrp0wsFsVkYAe0sbE",
+      "database": "bptswslxnt8j94cblpyh",
+      "host": "bptswslxnt8j94cblpyh-mysql.services.clever-cloud.com",
+      "dialect": "mysql"
+    },
+    "test": {
+      "username": "root",
+      "password": "",
+      "database": "infiori",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    },
+    "production": {
+      "username": "root",
+      "password": "",
+      "database": "infiori",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    }
+  };
